a96f28f3264c711546b124811e39153a451d8b98
Peter Forrest <petergforrest91@gmail.com>
2017-11-10 17:04:11 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace InterviewQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str = "Short string";
+            //char[] cArray = Str.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+
+            // var length = cArray.Length;
+            for (int i = Str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(Str[i]);
+            }
+            Console.WriteLine(sb);
+
+        }
+    }
+}
