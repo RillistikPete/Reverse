8bb04bbbd0ef84095f3847e77564251bc111a0ea
Peter Forrest <petergforrest91@gmail.com>
2017-11-11 17:34:37 -0600

    readme agian

--- Program.cs
+++ Program.cs
@@ -8,16 +8,13 @@ namespace InterviewQ2
         static void Main(string[] args)
         {
             string Str = "Short string";
-            //char[] cArray = Str.ToCharArray();
+            //Decided not to use .ToCharArray()
             StringBuilder sb = new StringBuilder();
-
-            // var length = cArray.Length;
             for (int i = Str.Length - 1; i >= 0; i--)
             {
                 sb.Append(Str[i]);
             }
             Console.WriteLine(sb);  
-
         }
     }
 }
